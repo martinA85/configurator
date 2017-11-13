cc687fde1f672071716313a411e725f3bac4151f
Martin Allimonier <allimonier.martin@gmail.com>
2017-11-13 10:58:42 +0100

    js debug

--- configOdoo/static/js/config.js
+++ configOdoo/static/js/config.js
@@ -158,7 +158,7 @@ function updateConfigTab(variant_name){
   variant_tab.getElementsByClassName("variant_extra_recap")[0].innerHTML = extra;
 
   if(layout == "v"){
-    console.log("updateImg");
+    console.log(variant_img.src);
     variant_tab.getElementsByClassName("variant_img_recap")[0].src = variant_img.src;
   }
 
