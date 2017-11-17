c6fc7b8534d850f289df3164967571a9fe525d55
Martin Allimonier <allimonier.martin@gmail.com>
2017-11-17 09:23:49 +0100

    fixing price for adding a configured product to cart

--- configOdoo/models/models.py
+++ configOdoo/models/models.py
@@ -88,8 +88,8 @@ class SaleOrder(models.Model):
 
     @api.multi
     def _cart_update(self, product_id=None, line_id=None, add_qty=0, set_qty=0, attributes=None,config=None,**kwargs):
+		values = self._website_product_id_change(self.id, product_id, qty=order_line.product_uom_qty)
     	to_return = super(SaleOrder, self)._cart_update(product_id=int(product_id),add_qty=add_qty, set_qty=set_qty)
-    	values = self._website_product_id_change(self.id, product_id, qty=order_line.product_uom_qty)
     	order_line = self._cart_find_product_line(product_id, line_id, **kwargs)
     	product = self.env['product.product'].browse(product_id)
 
