a4657dc154eaf682ca6426a77064483b5d7ab4c2
Martin Allimonier <allimonier.martin@gmail.com>
2017-11-21 16:02:39 +0100

    ui improvement

--- configOdoo/__manifest__.py
+++ configOdoo/__manifest__.py
@@ -1,23 +1,25 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Configurateur de produit",
+    'name': "Odoo product configurator",
     'js': ['static/js/config.js'],
     'test': ['static/js/config.js'],
+    'category' : 'Website'
     'summary': """
-        Permet de configurer un produit""",
+        Allow you to add a dynamic product configurator on your website""",
 
     'description': """
-        Long description of module's purpose
+        This module allows you to add dynamics product configurator for you'r products web page. You can allow your customer to take order of your product with their configuration or jsut ask for a quotation.
+        This module his let ypu choice between two layout for you'r product : vertcal or horizontal.
     """,
 
-    'author': "Martin Allimonier",
+    'author': "Noosys",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'test',
-    'version': '0.1',
+    'version': '1.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base','product', 'website_sale', 'website', 'website_form','crm'],
@@ -35,4 +37,8 @@
     'demo': [
         # 'demo/demo.xml',
     ],
+    "price":100,
+    "currency":"EUR",
+    "installable":True,
+    "auto_install":False,
 }
