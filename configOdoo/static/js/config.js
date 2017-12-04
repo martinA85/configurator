301623c3af930357017e5ea57451fe3357baaacf
Martin Allimonier <allimonier.martin@gmail.com>
2017-12-04 09:49:18 +0100

    fixing delete button on h summary tab

--- configOdoo/static/js/config.js
+++ configOdoo/static/js/config.js
@@ -173,7 +173,7 @@ function updateConfigTab(variant_name){
 //function that clear configuration (activated by del button on summary tab)
 function clearConfig(){
 
-  var variant_name = this.parentElement.parentElement.getElementsByClassName("variant_name_recap");
+  var variant_name = this.parentElement.parentElement.getElementsByClassName("variant_name_recap").value;
 
   //first step : delete variant layer on the product image
   layer_name = "lay_"+variant_name;
